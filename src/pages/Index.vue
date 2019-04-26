ec424e82a8c55f4bc275160659825e6db6e90923
kostia7alania <osetia-alania@mail.ru>
2019-04-26 20:36:56 +0300

    add inputs

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,10 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center column ">
 
-    <h1>
-      Пагнале
-    </h1>
+    <h1> Пагнале </h1>
+    <q-input v-model="number" stack-label="Введите номер" placeholder="FNRU/160419/0181/01"/>
+    <q-input v-model="bd" float-label="Введите дату рождения" placeholder="26/12/1992" />
+
   </q-page>
 </template>
 
@@ -11,7 +12,15 @@
 </style>
 
 <script>
+
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      number: 'FNRU/160419/0181/01',
+      bd: '26/12/1992'
+    }
+  }
+
 }
 </script>
